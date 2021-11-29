92c24905602d6be9c5904200728c0ddc2f27a79e
conspir0 <michalczyk.mat3usz@gmail.com>
2021-11-29 12:43:51 +0100

    Add basic view components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World!
+        <Navbar/>
+        <Home/>
+        <About/>
+        <Contact/>
       </div>
     );
   };
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+    <h4 className="center">Home</h4>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam adipisci nostrum dolorem aspernatur! Debitis voluptate alias, sunt minima cum iste hic. Fugit ex eos aspernatur esse alias beatae ut sequi.</p>
+  </div>
+  );
+};
+
+export default Home;
