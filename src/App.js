096bfdc4ea40e61476dee414d98b169ef5021754
conspir0 <michalczyk.mat3usz@gmail.com>
2021-11-29 13:47:51 +0100

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import About from './components/About';
 import Contact from './components/Contact';
 import Home from './components/Home';
@@ -7,12 +9,16 @@ import Navbar from './components/Navbar';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar/>
-        <Home/>
-        <About/>
-        <Contact/>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+          </Routes>
+        </div>
+      </Router>
     );
   };
 }
